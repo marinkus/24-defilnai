d1c7177a02373644b3d83d10e0811502262cf5dc
marinkus <marinkus@gmail.com>
2022-08-02 14:12:03 +0300

    012

--- 012/index.php
+++ 012/index.php
@@ -33,4 +33,61 @@ $one = $suma - (2 * $two);
 $zero = 4 - $one - $two;
 
 echo "$_1 $_2 $_3 $_4 ----- $zero : $one : $two";
+echo '<br>';
 
+$drambliai = 3;
+$raganosiai = 2;
+$begemotai = 6;
+if ($begemotai > $raganosiai && $begemotai > $drambliai) echo 'Begemotų yra daugiausiai';
+echo '<br>';
+echo '<br>';
+
+// 6 > 2 && 6 > 3;
+// true && true
+// true
+
+$a = 5 > 9 ? 'Jo' : 'Labai abejotina';
+
+echo $a;
+echo '<br>';
+
+// $i=1;
+// var_dump(isset($i)); // gražina FALSE
+// isset($i); // gražina TRUE
+// $i=null; 
+// isset($i); //gražina FALSE
+
+
+$rezultatas = $vienas ?? 8; // gražina 8
+$vienas = 1;
+$rezultatas = $vienas ?? 8; // gražina 1
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+$siuntinukoDydis = ['S', 'M', 'L', 'XL'][rand(0, 3)];
+echo "Senukas atnešė $siuntinukoDydis dydžio siuntą.";
+
+// if ($siuntinukoDydis == 'S') {
+//     echo '<br>Tikrinam S';
+// }
+// if ($siuntinukoDydis == 'S' || $siuntinukoDydis == 'M') {
+//     echo '<br>Tikrinam M';
+// }
+// if ($siuntinukoDydis == 'S' || $siuntinukoDydis == 'M' || $siuntinukoDydis == 'L') {
+//     echo '<br>Tikrinam L';
+// }
+// if ($siuntinukoDydis == 'S' || $siuntinukoDydis == 'M' || $siuntinukoDydis == 'L' || $siuntinukoDydis == 'XL') {
+//     echo '<br>Tikrinam XL';
+// }
+
+switch ($siuntinukoDydis) {
+    case 'S':
+        echo "<br>Tikriname S";
+    case 'M':
+        echo "<br>Tikriname M";
+    case 'L':
+        echo "<br>Tikriname L";
+    case 'XL':
+        echo "<br>Tikriname XL";
+};
