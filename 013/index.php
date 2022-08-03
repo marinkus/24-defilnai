9613e1f8951eb1b145e2a91da696547f476fc383
marinkus <marinkus@gmail.com>
2022-08-03 14:30:25 +0300

    013

--- 013/index.php
+++ 013/index.php
@@ -1,1 +1,21 @@
 <?php
+
+echo '<pre>';
+
+
+// for ($i = rand(0, 25); $i != 10; $i=rand(0, 25)) {
+//     echo "Dabar: $i \n";
+// };
+
+$x = 0;
+
+do {
+    echo "Dabar: $x \n";
+    $x++;
+    $y = 0;
+    
+    while ($y < 5) {
+        echo "O Dabar: $y \n";
+        $y++;
+    };
+} while ($x < 7);
