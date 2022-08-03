caf6bb9065d9aa62f4136183e65960d85fa33d38
marinkus <marinkus@gmail.com>
2022-08-03 09:04:06 +0300

    3-8 HW

--- 012/strings.php
+++ 012/strings.php
@@ -21,18 +21,58 @@ echo '<br>';
 echo strtolower($actorSurname);
 
 /* 3. Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus. Sukurti trečią kintamąjį ir jam priskirti stringą, sudarytą iš pirmų vardo ir pavardės kintamųjų raidžių. Jį atspausdinti. */
+
 echo '<br><br> Uzduotis nr. 3 --------------- <br>';
+$fullActorName = $actorName . ' ' . $actorSurname;
+echo $fullActorName;
 
 /* 4. Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus. Sukurti trečią kintamąjį ir jam priskirti stringą, sudarytą iš trijų paskutinių vardo ir pavardės kintamųjų raidžių. Jį atspausdinti. */
+
 echo '<br><br> Uzduotis nr. 4 --------------- <br>';
+
+$mixedName = $actorName[strlen($actorName)-3].$actorName[strlen($actorName)-2].$actorName[strlen($actorName)-1].$actorSurname[strlen($actorSurname)-3].$actorSurname[strlen($actorSurname)-2].$actorSurname[strlen($actorSurname)-1];
+echo $mixedName;
+
 /* 5. Sukurti kintamąjį su stringu: “An American in Paris”. Jame visas “a” (didžiąsias ir mažąsias) pakeisti žvaigždutėm “*”.  Rezultatą atspausdinti. */
+
 echo '<br><br> Uzduotis nr. 5 --------------- <br>';
+$anAmericanInParis = 'An American in Paris';
+echo strtr($anAmericanInParis, ['a' => '*', 'A' => '*']);
+
 /* 6. Sukurti kintamąjį su stringu: “An American in Paris”. Suskaičiuoti visas “a” (didžiąsias ir mažąsias) raides. Rezultatą atspausdinti. */
+
 echo '<br><br> Uzduotis nr. 6 --------------- <br>';
+$letterCount = 0;
+for ($i = 0; $i < strlen($anAmericanInParis); $i++) {
+   if ($anAmericanInParis[$i] == 'a' || $anAmericanInParis[$i] == 'A') {
+    $letterCount++;
+   };
+};
+echo $letterCount;
+
+
 /* 7. Sukurti kintamąjį su stringu: “An American in Paris”. Jame ištrinti visas balses. Rezultatą atspausdinti. Kodą pakartoti su stringais: “Breakfast at Tiffany's”, “2001: A Space Odyssey” ir “It's a Wonderful Life”. */
+
 echo '<br><br> Uzduotis nr. 7 --------------- <br>';
+
+echo preg_replace('#[aeiou\s]+#i', '', $anAmericanInParis);
+
+$movieName1 = "Breakfast at Tiffany's";
+$movieName2 = '2001: A Space Odyssey';
+$movieName3 = "It's a Wonderful Life";
+echo preg_replace('#[aeiouy\s]+#i', '', $movieName1);
+echo preg_replace('#[aeiouy\s]+#i', '', $movieName2);
+echo preg_replace('#[aeiouy\s]+#i', '', $movieName3);
+
 /* 8. Stringe, kurį generuoja toks kodas: 'Star Wars: Episode '.str_repeat(' ', rand(0,5)). rand(1,9) . ' - A New Hope'; Surasti ir atspausdinti epizodo numerį.*/
+
 echo '<br><br> Uzduotis nr. 8 --------------- <br>';
+
+$starWars = 'Star Wars: Episode '.str_repeat(' ', rand(0,5)). rand(1,9) . ' - A New Hope';
+
+echo "$starWars <br>";
+echo preg_replace('/[^0-9]/', '', $starWars);
+
 /* 9. Suskaičiuoti kiek stringe “Don't Be a Menace to South Central While Drinking Your Juice in the Hood” yra žodžių trumpesnių arba lygių nei 5 raidės. Pakartokite kodą su stringu “Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale”. */
 echo '<br><br> Uzduotis nr. 9 --------------- <br>';
 /* 10. Parašyti kodą, kuris generuotų atsitiktinį stringą iš lotyniškų mažųjų raidžių. Stringo ilgis 3 simboliai. */
