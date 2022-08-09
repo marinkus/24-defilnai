c60500a70e1abd114b1043aeff5f37d0691a1d46
marinkus <marinkus@gmail.com>
2022-08-09 07:59:50 +0300

    first hw commit, 1-2

---
+++ 015/functions.php
@@ -0,0 +1,53 @@
+<h2>Homework: Funkcijos</h2>
+
+<?php
+
+// 1. Parašykite funkciją, kurios argumentas būtų tekstas, kuris yra įterpiamas į h1 tagą;
+echo '<br><br>Uzduotis nr. 1 ----- <br><br>';
+
+function headingConstructor($a = 'Heading') {
+    return "<h1>$a</h2>";
+}
+
+// echo headingConstructor('Labas rytas');
+
+
+// 2. Parašykite funkciją su dviem argumentais, pirmas argumentas tekstas, įterpiamas į h tagą, o antrasis tago numeris (1-6). Rašydami šią funkciją remkitės pirmame uždavinyje parašytą funkciją;
+echo '<br><br>Uzduotis nr. 2 ----- <br><br>';
+
+function headingGenerator($text, $tag) {
+    return "<h$tag>$text</h$tag>";
+}
+
+// echo headingGenerator('Lbbbbbbbbb', 3);
+
+// 3. Generuokite atsitiktinį stringą, pasinaudodami kodu md5(time()). Visus skaitmenis stringe įdėkite į h1 tagą. Raides palikite. Jegu iš eilės eina keli skaitmenys, juos į tagą reikią dėti kartu (h1 atsidaro prieš pirmą ir užsidaro po paskutinio) Keitimui naudokite pirmo patobulintą (jeigu reikia) uždavinio funkciją ir preg_replace_callback();
+echo '<br><br>Uzduotis nr. 3 ----- <br><br>';
+
+
+// 4. Parašykite funkciją, kuri skaičiuotų, iš kiek sveikų skaičių jos argumentas dalijasi be liekanos (išskyrus vienetą ir patį save) Argumentą užrašykite taip, kad būtų galima įvesti tik sveiką skaičių;
+echo '<br><br>Uzduotis nr. 4 ----- <br><br>';
+
+
+// 5. Sugeneruokite masyvą iš 100 elementų, kurio reikšmės atsitiktiniai skaičiai nuo 33 iki 77. Išrūšiuokite masyvą pagal daliklių be liekanos kiekį mažėjimo tvarka, panaudodami ketvirto uždavinio funkciją.
+echo '<br><br>Uzduotis nr. 5 ----- <br><br>';
+
+
+// 6. Sugeneruokite masyvą iš 100 elementų, kurio reikšmės atsitiktiniai skaičiai nuo 333 iki 777. Naudodami 4 uždavinio funkciją iš masyvo ištrinkite pirminius skaičius.
+echo '<br><br>Uzduotis nr. 6 ----- <br><br>';
+
+
+// 7. Sugeneruokite atsitiktinio (nuo 10 iki 20) ilgio masyvą, kurio visi, išskyrus paskutinį, elementai yra atsitiktiniai skaičiai nuo 0 iki 10, o paskutinis masyvas, kuris generuojamas pagal tokią pat salygą kaip ir pirmasis masyvas. Viską pakartokite atsitiktinį nuo 10 iki 30  kiekį kartų. Paskutinio masyvo paskutinis elementas yra lygus 0;
+echo '<br><br>Uzduotis nr. 7 ----- <br><br>';
+
+
+// 8. Suskaičiuokite septinto uždavinio elementų, kurie nėra masyvai, sumą. Skaičiuoti reikia visuose masyvuose ir submasyvuose.
+echo '<br><br>Uzduotis nr. 8 ----- <br><br>';
+
+
+// 9. Sugeneruokite masyvą iš trijų elementų, kurie yra atsitiktiniai skaičiai nuo 1 iki 33. Jeigu tarp trijų paskutinių elementų yra nors vienas ne pirminis skaičius, prie masyvo pridėkite dar vieną elementą- atsitiktinį skaičių nuo 1 iki 33. Vėl patikrinkite pradinę sąlygą ir jeigu reikia pridėkite dar vieną elementą. Kartokite, kol sąlyga nereikalaus pridėti elemento. 
+echo '<br><br>Uzduotis nr. 9 ----- <br><br>';
+
+
+// 10. Sugeneruokite masyvą iš 10 elementų, kurie yra masyvai iš 10 elementų, kurie yra atsitiktiniai skaičiai nuo 1 iki 100. Jeigu tokio didelio masyvo (ne atskirai mažesnių) pirminių skaičių vidurkis mažesnis už 70, suraskite masyve mažiausią skaičių (nebūtinai pirminį) ir prie jo pridėkite 3. Vėl paskaičiuokite masyvo pirminių skaičių vidurkį ir jeigu mažesnis nei 70 viską kartokite. 
+echo '<br><br>Uzduotis nr. 10 ----- <br><br>';
