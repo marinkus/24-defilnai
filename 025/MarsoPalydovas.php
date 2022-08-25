ebe0eadccab3f260b140621c3d187eeeeb71fa57
marinkus <marinkus@gmail.com>
2022-08-25 12:34:10 +0300

    marso palydovai

---
+++ 025/MarsoPalydovas.php
@@ -0,0 +1,25 @@
+<?php
+namespace Kosmosas;
+
+
+class MarsoPalydovas {
+    const PALYDOVAI = ['Fobas', 'Deimas'];
+    private static $palydovai = [null, null];
+    public $title;
+
+    static function create() {
+        if (self::$palydovai[0] === null) {
+            return self::$palydovai[0] = new self(self::PALYDOVAI[0]);
+        } else if (self::$palydovai[1] === null) {
+            return self::$palydovai[1] = new self(self::PALYDOVAI[1]);
+        } else {
+            return self::$palydovai[rand(0, 1)];
+        }
+    }
+
+
+    private function __construct($title) {
+        $this->title = $title;
+    }
+
+}
---
+++ 025/index.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/MarsoPalydovas.php';
+
+
+$palydovas1 = Kosmosas\MarsoPalydovas::create();
+$palydovas2 = Kosmosas\MarsoPalydovas::create();
+$palydovas3 = Kosmosas\MarsoPalydovas::create();
+$palydovas4 = Kosmosas\MarsoPalydovas::create();
+
+print_r($palydovas1);
+print_r($palydovas2);
+print_r($palydovas3);
+print_r($palydovas4);
