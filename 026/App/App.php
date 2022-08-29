b1b80c607fe962981b598da7d8a1946b9b635ed3
marinkus <marinkus@gmail.com>
2022-08-29 13:12:17 +0300

    commit #026

---
+++ 026/App/App.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use App\Controllers\HomeController as HC;
+
+class App {
+
+
+    static public function start() {
+        self::router();
+    }
+
+
+
+    static public function router() {
+        $url = $_SERVER['REQUEST_URI'];
+        $url = explode('/', $url);
+        array_shift($url);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method == 'GET' && count($url) == 1 && $url[0] == '') {
+            return ((new HC) -> home());
+        }
+    }
+
+
+}
---
+++ 026/public/index.php
@@ -0,0 +1,8 @@
+<?php
+use App\App;
+
+define('DIR', __DIR__ . '/../');
+
+require(DIR . 'vendor/autoload.php');
+
+App::start();
