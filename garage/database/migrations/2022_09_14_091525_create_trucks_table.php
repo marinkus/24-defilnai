dce25b9e63eb1f90506948c059320f0352bd3562
marinkus <marinkus@gmail.com>
2022-09-14 12:28:18 +0300

    db and models created

---
+++ garage/database/migrations/2022_09_14_091525_create_trucks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trucks', function (Blueprint $table) {
+            $table->id();
+            $table->string('maker', 255);
+            $table->string('plate', 20);
+            $table->unsignedTinyInteger('make_year');
+            $table->text('mechanic_notices');
+            $table->unsignedBigInteger('mechanic_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trucks');
+    }
+};
