8fa0c98066e0db1ac3c0ec7de9a3b14caed54149
marinkus <marinkus@gmail.com>
2022-09-29 12:43:25 +0300

    initial commit for exam

---
+++ df1/database/migrations/2022_09_29_092233_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->decimal('price', 5, 2);
+            $table->float('rating', 4, 2)->nullable();
+            $table->unsignedBigInteger('rating_sum')->default(0);
+            $table->unsignedBigInteger('rating_count')->default(0);
+            $table->unsignedBigInteger('cat_id');
+            $table->foreign('cat_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
