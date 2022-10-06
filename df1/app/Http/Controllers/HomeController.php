cd06be189839824924a31a74d13e70b34221d2aa
marinkus <marinkus@gmail.com>
2022-10-06 13:43:02 +0300

    search 2 words

--- df1/app/Http/Controllers/HomeController.php
+++ df1/app/Http/Controllers/HomeController.php
@@ -14,31 +14,31 @@ class HomeController extends Controller
         // Filter
         if ($request->cat) {
             $movies = Movie::where('category_id', $request->cat);
-        }
-        else {
+        } else {
             $movies = Movie::where('id', '>', 0);
         }
 
         // Sort & search
         if ($request->sort == 'rate_asc') {
             $movies->orderBy('rating');
-        }
-        else if ($request->s) {
-            $movies = Movie::where('title', 'like', '%' . $request->s . '%');
-        }
-        else if ($request->sort == 'rate_desc') {
+        } else if ($request->s) {
+
+            $search = explode(' ', $request->s);
+            if (count($search) == 1) {
+                $movies = Movie::where('title', 'like', '%' . $request->s . '%');
+            } else {
+                $movies = Movie::where('title', 'like', '%' . $search[0] . '%' . $search[1] . '%')
+                ->orWhere('title', 'like', '%' . $search[1] . '%' . $search[0] . '%');
+            }
+        } else if ($request->sort == 'rate_desc') {
             $movies->orderBy('rating', 'desc');
-        }
-        else if ($request->sort == 'title_asc') {
+        } else if ($request->sort == 'title_asc') {
             $movies->orderBy('title');
-        }
-        else if ($request->sort == 'title_asc') {
+        } else if ($request->sort == 'title_asc') {
             $movies->orderBy('title', 'desc');
-        }
-        else if ($request->sort == 'price_asc') {
+        } else if ($request->sort == 'price_asc') {
             $movies->orderBy('price');
-        }
-        else if ($request->sort == 'price_asc') {
+        } else if ($request->sort == 'price_asc') {
             $movies->orderBy('price', 'desc');
         }
 
@@ -58,10 +58,9 @@ class HomeController extends Controller
     public function rate(Request $request, Movie $movie)
     {
         $movie->rating_sum = $movie->rating_sum + $request->rate;
-        $movie->rating_count ++;
+        $movie->rating_count++;
         $movie->rating = $movie->rating_sum / $movie->rating_count;
         $movie->save();
         return redirect()->back();
     }
-
 }
